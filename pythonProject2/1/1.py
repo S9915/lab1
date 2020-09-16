b9ad3dfe346d22413f7019ecd12948c3a2ca1e58
9915 <52491712+S9915@users.noreply.github.com>
2020-09-16 20:04:52 +0300

    Add files via upload

---
+++ pythonProject2/1/1.py
@@ -0,0 +1,6 @@
+def array_to_dict(array: list) -> dict:
+    return {array[i]: [item[0] for item in list(enumerate(array)) if item[1] == array[i]] for i in range(len(array))}
+
+
+if __name__ == '__main__':
+    print(array_to_dict(['a', 'b', 'c', 'd', 'e', 'a', 'a', 'b', 'c']))
---
+++ pythonProject2/2/2.py
@@ -0,0 +1,13 @@
+def jaccard_similarity(set1: set, set2: set) -> float:
+    intersec_size = 0
+    union_size = len(set1) + len(set2)
+    for item in set1:
+        if item in set2:
+            intersec_size += 1
+            union_size -= 1
+
+    return intersec_size / union_size
+
+
+if __name__ == '__main__':
+    print("Метрика Жаккарда: " + str(jaccard_similarity({1, 2, 3}, {2, 4, 5})))
