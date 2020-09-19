56a813979dc1ec58cadbc6568a106d1f5ba153a0
9915 <52491712+S9915@users.noreply.github.com>
2020-09-19 15:25:26 +0300

    Update 1.py

--- lab1/1/1.py
+++ lab1/1/1.py
@@ -1,5 +1,4 @@
-def array_to_dict(array: list) -> dict:
+def ardic(array):
     return {array[i]: [item[0] for item in list(enumerate(array)) if item[1] == array[i]] for i in range(len(array))}
 
-if __name__ == '__main__':
-    print(array_to_dict(['a', 'b', 'c', 'd', 'e', 'a', 'a', 'b', 'c']))
+ardic(['a', 'b', 'c', 'd', 'e', 'a', 'a', 'b', 'c'])
