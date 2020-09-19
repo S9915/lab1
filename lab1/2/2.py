f9f7990749af844f79c4147d0ca2e243ea6d5a9e
9915 <52491712+S9915@users.noreply.github.com>
2020-09-19 15:25:47 +0300

    Update 2.py

--- lab1/2/2.py
+++ lab1/2/2.py
@@ -1,16 +1,15 @@
-def jaccard(set1: set, set2: set) -> float:
-    intersec_size = 0
-    union_size = len(set1) + len(set2)
+def jaccar(set1, set2):
+    union = len(set1) + len(set2)
+    inter = 0
     for item in set1:
         if item in set2:
-            intersec_size += 1
-            union_size -= 1
-    return intersec_size / union_size
+            inter += 1
+            union -= 1
+    return inter / union
 
-if __name__ == '__main__':
-    import random as rnd
-    for i in range(1):
-        set1 = set([rnd.randint(1, 15) for i in range(rnd.randint(5, 10))])
-        set2 = set([rnd.randint(1, 15) for i in range(rnd.randint(5, 10))])
-        print(f"set 1: {set1}", f"set 2: {set2}", '', sep='\n')
-    print("Метрика Жаккарда: " + str({round(jaccard(set1, set2), 3)}))
+import random as rnd
+for i in range(3):
+    set1 = set([rnd.randint(1, 15) for i in range(rnd.randint(5, 10))])
+    set2 = set([rnd.randint(1, 15) for i in range(rnd.randint(5, 10))])
+    print(f"set 1: {set1}", f"set 2: {set2}", '', sep='\n')
+    print("Метрика Жаккара: " + str({round(jaccar(set1, set2), 4)}), '\n')
